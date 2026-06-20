ebe90fb4362c550f9ed896a0a296ba1534af0e36
Richie Rhein <richardrhein2@gmail.com>
2026-06-20 02:48:20 -0500

    routed processors to STD and added conditional node install based on release version

--- ct/foundryvtt.sh
+++ ct/foundryvtt.sh
@@ -73,4 +73,4 @@ description
 msg_ok "Completed successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW}Access it using the following URL:${CL}"
-echo -e "${GATEWAY}${BGN}https://${IP}:30000${CL}"
+echo -e "${GATEWAY}${BGN}http://${IP}:30000${CL}"
--- install/foundryvtt-install.sh
+++ install/foundryvtt-install.sh
@@ -13,50 +13,37 @@ setting_up_container
 network_check
 update_os
 
-msg_info "Installing Node.js"
-NODE_VERSION="24" setup_nodejs
-msg_ok "Node.js installed"
+# msg_info "Installing Node.js"
+# NODE_VERSION="24" setup_nodejs
+# msg_ok "Node.js installed"
 
 # create_self_signed_cert
 
 msg_info "Installing FoundryVTT"
 base_install_dir="/opt/foundryvtt"
-release_version_regex='^.*releases\/(\d+.\d+).*$'
+release_version_regex='^.*releases\/(\d+).(\d+).*$'
 
 cd /opt
-# mkdir -p /opt/actualbudget-data/{server-files,upload,migrate,user-files,migrations,config}
-# chown -R root:root /opt/actualbudget-data
-# chmod -R 755 /opt/actualbudget-data
 
-# cat <<EOF >/opt/actualbudget-data/config.json
-# {
-#   "port": 5006,
-#   "hostname": "::",
-#   "serverFiles": "/opt/actualbudget-data/server-files",
-#   "userFiles": "/opt/actualbudget-data/user-files",
-#   "trustedProxies": [
-#     "10.0.0.0/8",
-#     "172.16.0.0/12",
-#     "192.168.0.0/16",
-#     "127.0.0.0/8",
-#     "::1/128",
-#     "fc00::/7"
-#   ],
-#   "https": {
-#     "key": "/etc/ssl/actualbudget/actualbudget.key",
-#     "cert": "/etc/ssl/actualbudget/actualbudget.crt"
-#   }
-# }
-# EOF
 mkdir -p "/opt/foundryvtt/foundryvtt"
 mkdir -p "/opt/foundryvtt/foundrydata"
 cd "/opt/foundryvtt/foundryvtt"
-# $STD npm install --location=global @actual-app/sync-server
-wget -q "$DOWNLOAD_URL"
-unzip FoundryVTT*.zip*
-# [[ $DOWNLOAD_URL =~ $release_version_regex ]]
-# RELEASE=${BASH_REMATCH[1]}
-# echo "${RELEASE}" >~/.foundryvtt
+$STD wget -q "$DOWNLOAD_URL"
+$STD unzip FoundryVTT*.zip*
+$STD rm FoundryVTT*.zip*
+[[ $DOWNLOAD_URL =~ $release_version_regex ]]
+if [ ${BASH_REMATCH[1]} -ge 14 ]
+then
+    msg_info "Installing Node.js"
+    NODE_VERSION="24" setup_nodejs
+    msg_ok "Node.js installed"
+else
+    msg_info "Installing Node.js"
+    NODE_VERSION="22" setup_nodejs
+    msg_ok "Node.js installed"
+fi
+RELEASE="${BASH_REMATCH[1]}.${BASH_REMATCH[2]}"
+echo "${RELEASE}" >/opt/foundryvtt/.release
 msg_ok "Installed FoundryVTT"
 
 msg_info "Creating Service"
